aa8b2f92b43132306d5900eeaa6c7bca18f18d9e
Mike Dunn <mdunn@safaribooksonline.com>
2017-09-06 14:49:38 -0500

    commented

--- oreilly-annotator.js
+++ oreilly-annotator.js
@@ -64,26 +64,25 @@ Annotator.prototype = {
     var highlights = this.options.rootNode.querySelectorAll(this.options.tagName + '.' + this.options.className);
     Array.from(highlights).forEach(this.clearHighlight.bind(this));
   },
-
+  
   /**
    * By passing an Annotation object, this will highlight all elements from the start and end positions,
    * both described using XPath and character offsets.
    */
   highlightAnnotation: function(annotation) {
 
-    var startElement = this.findElementFromXpath(annotation.start);
-    var endElement = this.findElementFromXpath(annotation.end);
+    var start = this.findElementFromXpath(annotation.start);
+    start = this.getNodeAndOffset(start, annotation.startOffset);
+    start = start.node.splitText(start.offset);
+    
+    var end = this.findElementFromXpath(annotation.end);
+    end = this.getNodeAndOffset(end, annotation.endOffset);
+    end = end.node.splitText(end.offset).previousSibling;
 
-    var startNodeAndOffset = this.getNodeFromOffset(startElement, annotation.startOffset);
-    var startNode = startNodeAndOffset.node.splitText(startNodeAndOffset.offset);
-
-    var endNodeAndOffset = this.getNodeFromOffset(endElement, annotation.endOffset);
-    var endNode = endNodeAndOffset.node.splitText(endNodeAndOffset.offset).previousSibling;
-
-    var nodesBetween = this.getTextNodesBetween(startNode, endNode);
+    var between = this.getTextNodesBetween(start, end);
     var dotted = false;  // only show the dot on the first of a series
 
-    nodesBetween.forEach(function(node){
+    between.forEach(function(node){
       var highlighted = this.highlightNode(node);
       if (highlighted == null) {
         return true;
@@ -99,6 +98,9 @@ Annotator.prototype = {
     }, this);
   },
 
+  /**
+   * Given a node to start and end with, returns an array of all text nodes between.
+   */
   getTextNodesBetween : function(start, end) {
     var collection = [];
     var walker = document.createTreeWalker(this.options.rootNode, NodeFilter.SHOW_TEXT);
@@ -120,6 +122,10 @@ Annotator.prototype = {
     return collection;
   },
 
+  /**
+   * Returns true if the node represented by parameter "a" is earlier in the DOM
+   * than the node represented by parameter "b"
+   */
   nodeIsBefore : function(a, b) {
     var walker = document.createTreeWalker(this.options.rootNode);
     while (walker.nextNode()) {
@@ -136,7 +142,17 @@ Annotator.prototype = {
     return false;
   },
 
-  getNodeFromOffset: function(element, offset) {
+  /**
+   * Returns an object representing a text node and a character offset,
+   * by supplying an element and an offset.
+   * The node returned is the first node within element after as many characters
+   * as the offset parameter; the offset returned is the remaining offset.
+   * Thus, if spaces represent boundaries of text nodes, then
+   * if given "<div>01234 56789</div>" as the element and an offset of 7,
+   * this method would return the node that is the last child of the div,
+   * and a remaining offset of 2.
+   */
+  getNodeAndOffset: function(element, offset) {
     var count = 0;
     var walker = document.createTreeWalker(element, NodeFilter.SHOW_TEXT);
     while (walker.nextNode()) {
@@ -153,6 +169,9 @@ Annotator.prototype = {
     return null;
   },
 
+  /**
+   * Returns the element found at the given xpath, or null.
+   */
   findElementFromXpath: function(xpath) {
     var query = document.evaluate(xpath, this.options.rootNode, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
     if (query != null) {
@@ -161,6 +180,12 @@ Annotator.prototype = {
     return null;
   },
 
+  /**
+   * Returns a 1-based index (suitable for XPath indices) of en element,
+   * relative to siblings with like node name.  For example, if a div contained
+   * 2 paragraphs, a form, a table, a rule, and another paragraph, the last element would
+   * return child index of 3, as it is the 3rd of 3 paragraph elements in it's immediate DOM
+   */
   getLikeSiblingIndex: function(element) {
     var position = 1;
     var name = element.nodeName;
@@ -174,11 +199,14 @@ Annotator.prototype = {
     return position;
   },
 
+  /**
+   * Returns an XPath string expression, using only tag names and indices,
+   * from the given element, to options.rootNode
+   */
   getXPath: function(element) {
     if (element.nodeType != Node.ELEMENT_NODE) {
       element = element.parentElement;
     }
-    console.log(element);
     var tags = [];
     while (element != null && element != this.options.rootNode) {
       var index = this.getLikeSiblingIndex(element);
@@ -189,6 +217,10 @@ Annotator.prototype = {
     return tags.join(Annotator.XPATH_DELIMITER);
   },
 
+  /**
+   * Given a node and an optional ancestor (if omitted, defaults to the immediate parent
+   * of the node), will return the number of characters that precede the node in the ancestor.
+   */
   getOffsetToNode: function(node, parent) {
     if (arguments.length == 1) {
       parent = node.parentElement;
@@ -206,8 +238,8 @@ Annotator.prototype = {
   },
 
   /**
-  * get the first ancestor of the node that has the highlight class, or null
-  */
+   * Get the first ancestor of the node that has the highlight class, or null
+   */
   getHighlightAncestor: function(node) {
     while (node != null && node != this.options.rootNode) {
       if (node.nodeType == Node.ELEMENT_NODE && node.classList.contains(this.options.className)) {
@@ -219,8 +251,8 @@ Annotator.prototype = {
   },
 
   /**
-   * if the node is not in a highlight, return it
-   * otherwise return the first parent that is not a highlight
+   * If the node is not in a highlight, return it,
+   * otherwise return the first parent that is not in a highlight.
    */
   getNormalizedParent: function(node) {
     var highlight = this.getHighlightAncestor(node);
@@ -232,11 +264,11 @@ Annotator.prototype = {
 
 
   /**
-  * Selection gives us anchor/focusNode and anchor/focusOffset,
-  * which are really nice indicators of where the selection is.  Unfortunately,
-  * these need to persist, so we use the parent element of each node as xpath,
-  * and have to add the number of characters to the offset that leads to the node
-  */
+   * Selection gives us anchor/focusNode and anchor/focusOffset,
+   * which are really nice indicators of where the selection is.  Unfortunately,
+   * these need to persist, so we use the parent element of each node as xpath,
+   * and have to add the number of characters to the offset that leads to the node
+   */
   getAnnotationFromSelection: function(selection) {
     var startElement = this.getNormalizedParent(selection.anchorNode);
     var endElement = this.getNormalizedParent(selection.focusNode);
